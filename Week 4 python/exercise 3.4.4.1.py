f188fd664137d77094986b022314833a0b229556
Wijmar Snijder <snijderwijmar@gmail.com>
2020-09-30 13:26:10 +0200

    Ferdi g

---
+++ Week 4 python/exercise 3.4.4.1.py
@@ -0,0 +1,17 @@
+dag = int(input("Which day is it today"))
+
+if dag == 0:
+    print("sunday")
+if dag == 1:
+    print("monday")
+if dag == 2:
+    print("tuesday")
+if dag == 3:
+    print("wednesday")
+if dag == 4:
+    print("thursday")
+if dag == 5:
+    print("friday")
+if dag == 6:
+    print("saturday")
+
