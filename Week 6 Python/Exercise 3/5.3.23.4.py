eb88dcb081ea33091368b42cc05d67e90dac9d4d
Wijmar Snijder <snijderwijmar@gmail.com>
2020-10-15 15:51:06 +0200

    progress

---
+++ Week 6 Python/Exercise 3/5.3.23.4.py
@@ -0,0 +1,5 @@
+"""
+Test 1 is false, in this case this and that are two different objects that have the same value.
+Test 2 is true, this and that refer to the same object.
+
+"""
---
+++ Week 6 Python/Exercise 3/vectors.py
@@ -0,0 +1,8 @@
+def add_vectors(x, y):
+    newvec = [ ]
+    for i in range(len(x)):
+        newvec = newvec + [x[i]] + [y[i]]
+
+print(newvec)
+
+
